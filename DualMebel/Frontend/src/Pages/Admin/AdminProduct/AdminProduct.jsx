b9ce3313b5979d7cce11c4a9490c63a81d359c6a
aytekin789 <aytekinmemmadli@gmail.com>
2025-02-22 21:24:49 +0400

    frontend

--- DualMebel/Frontend/src/Pages/Admin/AdminProduct/AdminProduct.jsx
+++ DualMebel/Frontend/src/Pages/Admin/AdminProduct/AdminProduct.jsx
@@ -1,66 +1,72 @@
 import React, { useEffect, useState } from "react";
 import { Helmet } from "react-helmet-async";
+import { useNavigate } from "react-router-dom"; 
 
 export default function AdminProduct() {
-    const[product, setProduct] = useState([])
-   function GetData (){
-       fetch("http://localhost:3000/products")
-       .then((res)=>res.json())
-       .then(data=>setProduct(data))
-   }
-       useEffect(() => {
-        GetData();
-      }, []);
+    const [product, setProduct] = useState([]);
+    const navigate = useNavigate(); 
 
-      function deleteItemById(id) {
-        fetch("http://localhost:3000/products" + id, {
-          method: "DELETE",
+    function GetData() {
+        fetch("http://localhost:3000/products")
+            .then((res) => res.json())
+            .then(data => setProduct(data));
+    }
+
+    useEffect(() => {
+        GetData();
+    }, []);
+
+    function deleteItemById(id) {
+        fetch(`http://localhost:3000/products/${id}`, { 
+            method: "DELETE",
         }).then(() => GetData());
-      }
-  return (
-    <>
-      <Helmet>
-        <title>ADMIN</title>
-      </Helmet>
-      <table>
-        <thead>
-          <tr>
-            <th>IMAGE</th>
-            <th>TITLE</th>
-            <th>DESCRIPTION</th>
-            <th>CATEGORY</th>
-            <th>PRICE</th>
-            <th>DELETE</th>
-            <th>EDIT</th>
-          </tr>
-        </thead>
-        <tbody>
-          {product.map((x) => (
-            <tr key={x.id}>
-              <td style={{ border: "solid gray" }}>{x.image}</td>
-              <td style={{ border: "solid gray" }}>{x.title}</td>
-              <td style={{ border: "solid gray" }}>{x.description}</td>
-              <td style={{ border: "solid gray" }}>{x.category}</td>
-              <td style={{ border: "solid gray" }}>{x.price}AZN</td>
-              <td style={{ border: "solid gray" }}>
-                <button
-                  onClick={() => deleteItemById(x.id)}
-                  style={{ backgroundColor: "red" }}
-                >
-                  DELETE
-                </button>
-              </td>
-              <td style={{ border: "solid gray" }}>
-              
-                  <button
-                  onClick={() => navigate(`/edit/${x.id}`)} 
-                   style={{backgroundColor:"green" }}>EDIT</button>
-               
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </>
-  );
+    }
+
+    return (
+        <>
+            <Helmet>
+                <title>ADMIN</title>
+            </Helmet>
+            <table>
+                <thead>
+                    <tr>
+                        <th>IMAGE</th>
+                        <th>TITLE</th>
+                        <th>DESCRIPTION</th>
+                        <th>CATEGORY</th>
+                        <th>PRICE</th>
+                        <th>DELETE</th>
+                        <th>EDIT</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {product.map((x) => (
+                        <tr key={x.id}>
+                            <td style={{ border: "solid gray" }}>{x.image}</td>
+                            <td style={{ border: "solid gray" }}>{x.title}</td>
+                            <td style={{ border: "solid gray" }}>{x.description}</td>
+                            <td style={{ border: "solid gray" }}>{x.category}</td>
+                            <td style={{ border: "solid gray" }}>{x.price} AZN</td>
+                            <td style={{ border: "solid gray" }}>
+                                <button
+                                    onClick={() => deleteItemById(x.id)}
+                                    style={{ backgroundColor: "red" }}
+                                >
+                                    DELETE
+                                </button>
+                            </td>
+                            <td style={{ border: "solid gray" }}>
+                                <button
+                                    onClick={() => navigate(`/edit/${x.id}`)} 
+                                    style={{ backgroundColor: "green" }}
+                                >
+                                    EDIT
+                                </button>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </>
+    );
 }
